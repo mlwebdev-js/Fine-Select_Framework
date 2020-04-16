d79023e668ff7ae3f5394c6e1efeb9153859dcbe
mlwebdev-js <34560974+mlwebdev-js@users.noreply.github.com>
2020-04-15 21:38:26 -0400

    Update fs-framework.js

--- Fine-Select-Framework/fs-framework/fs-framework.js
+++ Fine-Select-Framework/fs-framework/fs-framework.js
@@ -1,47 +1,83 @@
 // fs-framework.js
 (function (
-  getId,            // document.getElementById()
-  getTag,           // document.getElementsByTagName()
-  getClass,         // document.getElementsByClassName()
-  getName,          // document.getElementsByName()
-  createEl,         // document.createElement()
-  setItem,          // localStorage.setItem()
-  setAttributes,    // Set multiple attributes to one element
-  addElementOnTag,  // Append child to parent with innerHTML
-  addElementOnId,   // Append child to parent with innerHTML
+  ID,               // document.getElementById()
+  TAG,              // document.getElementsByTagName()
+  CLASS,            // document.getElementsByClassName()
+  NAME,             // document.getElementsByName()
+  QUERY,            // document.querySelector()
+  QueryAll,         // // document.querySelectorAll()
+  CREATE,           // document.createElement()
+  StorageItem,      // localStorage.item_()
+  SetAttributes,    // Set multiple attributes to one element
+  AddElementOnTag,  // Append child to parent with innerHTML
+  AddElementOnId,   // Append child to parent with innerHTML
+  DynamicElements,  // Create dynamic Select options, li on ul and etc.
+  DoIf,             // Invoke a callback function if condition is true 
+  IfElse,           // Invoke a callback function if condition is true or a different callback function if condition is false
+  EvtHandle,        // Add EventHandle to variable / DOM element
+
   a_,abbr_,address_,area_,article_,aside_,audio_,b_,base_,bdi_,bdo_,blockquote_,body_,br_,button_,canvas_,caption_,cite_,code_,col_,colgroup_,data_,datalist_,dd_,del_,details_,dfn_,div_,dl_,dt_,em_,embed_,fieldset_,figcaption_,figure_,footer_,form_,h1_,h2_,h3_,h4_,h5_,h6_,head_,header_,hr_,html_,i_,iframe_,img_,input_,ins_,kbd_,label_,legend_,li_,link_,main_,map_,mark_,menu_,menuitem_,meta_,meter_,nav_,noframes_,noscript_,object_,ol_,optgroup_,option_,output_,p_,param_,pre_,progress_,q_,rp_,rt_,rtc_,ruby_,s_,samp_,script_,section_,select_,slot_,small_,source_,span_,strong_,style_,sub_,summary_,sup_,table_,tbody_,td_,template_,textarea_,tfoot_,th_,thead_,time_,title_,tr_,track_,u_,ul_,var_,video_,wbr_
   ) 
   {
-  this.getId = function(a) {
-    return document.getElementById(a)
-  }, this.getTag = function(a) {
-    return document.getElementsByTagName(a)[0]
-  }, this.getClass = function(a, i) {
-    return document.getElementsByClassName(a)[i]
-  }, this.getName = function(a) {
-    return document.getElementsByName(a)[0]
-  }, this.createEl = function(a) {
-    return document.createElement(a)
-  }, this.setItem = function(a, b) {
-    return localStorage.setItem(a, b)
-  }, this.setAttributes = function (el, attrs) {
-    for(var key in attrs) {
-      el.setAttribute(key, attrs[key]);
+     this.ID = function(a) {
+       return document.getElementById(a)
+  }, this.TAG = function(a) {
+       return document.getElementsByTagName(a)[0]
+  }, this.CLASS = function(a, i) {
+       return document.getElementsByClassName(a)[i]
+  }, this.NAME = function(a) {
+       return document.getElementsByName(a)[0]
+  }, this.QUERY = function(a) {
+       return document.querySelector(a)
+  }, this.QueryAll = function(a) {
+       return document.querySelectorAll(a)
+  }, this.CREATE = function(a) {
+       return document.createElement(a)
+  }, this.StorageItem = function(a, b) {
+       return localStorage.StorageItem(a, b)
+  }, this.SetAttributes = function (el, attrs) {
+       for(var key in attrs) {
+         el.setAttribute(key, attrs[key]);
+       } // Call like so --> setAttribute(varible, "id": "id_name")
+  }, this.AddElementOnTag = function (elp, elc, inp) {
+       elc = this.CREATE(elc)
+       elp = this.TAG(elp)
+        .appendChild(elc),
+       elc.innerHTML = inp;
+       return { elp, elc, inp }
+  }, this.AddElementOnId = function (elp, elc, inp) {
+       elc = this.CREATE(elc)
+       elp = this.ID(elp)
+        .appendChild(elc),
+       elc.innerHTML = inp;
+       return { elp, elc, inp }
+  }, this.a_=this.CREATE("a"),this.abbr_=this.CREATE("abbr"),this.address_=this.CREATE("address"),this.area_=this.CREATE("area"),this.article_=this.CREATE("article"),this.aside_=this.CREATE("aside"),this.audio_=this.CREATE("audio"),this.b_=this.CREATE("b"),this.base_=this.CREATE("base"),this.bdi_=this.CREATE("bdi"),this.bdo_=this.CREATE("bdo"),this.blockquote_=this.CREATE("blockquote"),this.body_=this.CREATE("body"),this.br_=this.CREATE("br"),this.button_=this.CREATE("button"),this.canvas_=this.CREATE("canvas"),this.caption_=this.CREATE("caption"),this.cite_=this.CREATE("cite"),this.code_=this.CREATE("code"),this.col_=this.CREATE("col"),this.colgroup_=this.CREATE("colgroup"),this.data_=this.CREATE("data"),this.datalist_=this.CREATE("datalist"),this.dd_=this.CREATE("dd"),this.del_=this.CREATE("del"),this.details_=this.CREATE("details"),this.dfn_=this.CREATE("dfn"),this.div_=this.CREATE("div"),this.dl_=this.CREATE("dl"),this.dt_=this.CREATE("dt"),this.em_=this.CREATE("em"),this.embed_=this.CREATE("embed"),this.fieldset_=this.CREATE("fieldset"),this.figcaption_=this.CREATE("figcaption"),this.figure_=this.CREATE("figure"),this.footer_=this.CREATE("footer"),this.form_=this.CREATE("form"),this.h1_=this.CREATE("h1"),this.h2_=this.CREATE("h2"),this.h3_=this.CREATE("h3"),this.h4_=this.CREATE("h4"),this.h5_=this.CREATE("h5"),this.h6_=this.CREATE("h6"),this.head_=this.CREATE("head"),this.header_=this.CREATE("header"),this.hr_=this.CREATE("hr"),this.html_=this.CREATE("html"),this.i_=this.CREATE("i"),this.iframe_=this.CREATE("iframe"),this.img_=this.CREATE("img"),this.input_=this.CREATE("input"),this.ins_=this.CREATE("ins"),this.kbd_=this.CREATE("kbd"),this.label_=this.CREATE("label"),this.legend_=this.CREATE("legend"),this.li_=this.CREATE("li"),this.link_=this.CREATE("link"),this.main_=this.CREATE("main"),this.map_=this.CREATE("map"),this.mark_=this.CREATE("mark"),this.menu_=this.CREATE("menu"),this.menuitem_=this.CREATE("menuitem"),this.meta_=this.CREATE("meta"),this.meter_=this.CREATE("meter"),this.nav_=this.CREATE("nav"),this.noframes_=this.CREATE("noframes"),this.noscript_=this.CREATE("noscript"),this.object_=this.CREATE("object"),this.ol_=this.CREATE("ol"),this.optgroup_=this.CREATE("optgroup"),this.option_=this.CREATE("option"),this.output_=this.CREATE("output"),this.p_=this.CREATE("p"),this.param_=this.CREATE("param"),this.pre_=this.CREATE("pre"),this.progress_=this.CREATE("progress"),this.q_=this.CREATE("q"),this.rp_=this.CREATE("rp"),this.rt_=this.CREATE("rt"),this.rtc_=this.CREATE("rtc"),this.ruby_=this.CREATE("ruby"),this.s_=this.CREATE("s"),this.samp_=this.CREATE("samp"),this.script_=this.CREATE("script"),this.section_=this.CREATE("section"),this.select_=this.CREATE("select"),this.slot_=this.CREATE("slot"),this.small_=this.CREATE("small"),this.source_=this.CREATE("source"),this.span_=this.CREATE("span"),this.strong_=this.CREATE("strong"),this.style_=this.CREATE("style"),this.sub_=this.CREATE("sub"),this.summary_=this.CREATE("summary"),this.sup_=this.CREATE("sup"),this.table_=this.CREATE("table"),this.tbody_=this.CREATE("tbody"),this.td_=this.CREATE("td"),this.template_=this.CREATE("template"),this.textarea_=this.CREATE("textarea"),this.tfoot_=this.CREATE("tfoot"),this.th_=this.CREATE("th"),this.thead_=this.CREATE("thead"),this.time_=this.CREATE("time"),this.title_=this.CREATE("title"),this.tr_=this.CREATE("tr"),this.track_=this.CREATE("track"),this.u_=this.CREATE("u"),this.ul_=this.CREATE("ul"),this.var_=this.CREATE("var"),this.video_=this.CREATE("video"),this.wbr__=this.CREATE("wbr");     
+  this.DoIf = function DoIf(condition, func) {
+    if(condition) {
+      func(); // Define a function replacement
     } 
-  }, this.addElementOnTag = function (elp, elc, inp) {
-       elc = this.createEl(elc)
-       elp = this.getTag(elp)
-        .appendChild(elc),
-       elc.innerHTML = inp;
-       return { elp, elc, inp }
-  }, this.addElementOnId = function (elp, elc, inp) {
-       elc = this.createEl(elc)
-       elp = this.getId(elp)
-        .appendChild(elc),
-       elc.innerHTML = inp;
-       return { elp, elc, inp }
-  }, this.a_=this.createEl("a"),this.abbr_=this.createEl("abbr"),this.address_=this.createEl("address"),this.area_=this.createEl("area"),this.article_=this.createEl("article"),this.aside_=this.createEl("aside"),this.audio_=this.createEl("audio"),this.b_=this.createEl("b"),this.base_=this.createEl("base"),this.bdi_=this.createEl("bdi"),this.bdo_=this.createEl("bdo"),this.blockquote_=this.createEl("blockquote"),this.body_=this.createEl("body"),this.br_=this.createEl("br"),this.button_=this.createEl("button"),this.canvas_=this.createEl("canvas"),this.caption_=this.createEl("caption"),this.cite_=this.createEl("cite"),this.code_=this.createEl("code"),this.col_=this.createEl("col"),this.colgroup_=this.createEl("colgroup"),this.data_=this.createEl("data"),this.datalist_=this.createEl("datalist"),this.dd_=this.createEl("dd"),this.del_=this.createEl("del"),this.details_=this.createEl("details"),this.dfn_=this.createEl("dfn"),this.div_=this.createEl("div"),this.dl_=this.createEl("dl"),this.dt_=this.createEl("dt"),this.em_=this.createEl("em"),this.embed_=this.createEl("embed"),this.fieldset_=this.createEl("fieldset"),this.figcaption_=this.createEl("figcaption"),this.figure_=this.createEl("figure"),this.footer_=this.createEl("footer"),this.form_=this.createEl("form"),this.h1_=this.createEl("h1"),this.h2_=this.createEl("h2"),this.h3_=this.createEl("h3"),this.h4_=this.createEl("h4"),this.h5_=this.createEl("h5"),this.h6_=this.createEl("h6"),this.head_=this.createEl("head"),this.header_=this.createEl("header"),this.hr_=this.createEl("hr"),this.html_=this.createEl("html"),this.i_=this.createEl("i"),this.iframe_=this.createEl("iframe"),this.img_=this.createEl("img"),this.input_=this.createEl("input"),this.ins_=this.createEl("ins"),this.kbd_=this.createEl("kbd"),this.label_=this.createEl("label"),this.legend_=this.createEl("legend"),this.li_=this.createEl("li"),this.link_=this.createEl("link"),this.main_=this.createEl("main"),this.map_=this.createEl("map"),this.mark_=this.createEl("mark"),this.menu_=this.createEl("menu"),this.menuitem_=this.createEl("menuitem"),this.meta_=this.createEl("meta"),this.meter_=this.createEl("meter"),this.nav_=this.createEl("nav"),this.noframes_=this.createEl("noframes"),this.noscript_=this.createEl("noscript"),this.object_=this.createEl("object"),this.ol_=this.createEl("ol"),this.optgroup_=this.createEl("optgroup"),this.option_=this.createEl("option"),this.output_=this.createEl("output"),this.p_=this.createEl("p"),this.param_=this.createEl("param"),this.pre_=this.createEl("pre"),this.progress_=this.createEl("progress"),this.q_=this.createEl("q"),this.rp_=this.createEl("rp"),this.rt_=this.createEl("rt"),this.rtc_=this.createEl("rtc"),this.ruby_=this.createEl("ruby"),this.s_=this.createEl("s"),this.samp_=this.createEl("samp"),this.script_=this.createEl("script"),this.section_=this.createEl("section"),this.select_=this.createEl("select"),this.slot_=this.createEl("slot"),this.small_=this.createEl("small"),this.source_=this.createEl("source"),this.span_=this.createEl("span"),this.strong_=this.createEl("strong"),this.style_=this.createEl("style"),this.sub_=this.createEl("sub"),this.summary_=this.createEl("summary"),this.sup_=this.createEl("sup"),this.table_=this.createEl("table"),this.tbody_=this.createEl("tbody"),this.td_=this.createEl("td"),this.template_=this.createEl("template"),this.textarea_=this.createEl("textarea"),this.tfoot_=this.createEl("tfoot"),this.th_=this.createEl("th"),this.thead_=this.createEl("thead"),this.time_=this.createEl("time"),this.title_=this.createEl("title"),this.tr_=this.createEl("tr"),this.track_=this.createEl("track"),this.u_=this.createEl("u"),this.ul_=this.createEl("ul"),this.var_=this.createEl("var"),this.video_=this.createEl("video"),this.wbr__=this.createEl("wbr");     
+  }, // call function like so --> DoIf(a == 1, newFunc)
+  this.IfElse = function IfElse(condition, func1, func2) {
+    if(condition) {
+      func1(); // Define a function replacement
+    } else {
+        func2();
+    }
+  }, // call function like so --> IfElse(a == 1, newFunc)
+  this.DynamicElements = function DynamicElements(el_target_id, array_name, child_el, child_text, el_target, child, i){
+    el_target = document.getElementById(el_target_id);
+    for(i = 0; i < array_name.length; i++) {
+      child_text = array_name[i];
+      child = document.createElement(child_el);
+      child.textContent = child_text;
+      child.value = child_text;
+      el_target.appendChild(child);
+    }
+  };
+  this.EvtHandle = function EvtHandle(vars, evt, func) {
+    return vars.addEventListener(evt, func)
+  } // call function like so --> EvtHandle(variable, "event", function);
    return { 
       a_,abbr_,address_,area_,article_,aside_,audio_,b_,base_,bdi_,bdo_,blockquote_,body_,br_,button_,canvas_,caption_,cite_,code_,col_,colgroup_,data_,datalist_,dd_,del_,details_,dfn_,div_,dl_,dt_,em_,embed_,fieldset_,figcaption_,figure_,footer_,form_,h1_,h2_,h3_,h4_,h5_,h6_,head_,header_,hr_,html_,i_,iframe_,img_,input_,ins_,kbd_,label_,legend_,li_,link_,main_,map_,mark_,menu_,menuitem_,meta_,meter_,nav_,noframes_,noscript_,object_,ol_,optgroup_,option_,output_,p_,param_,pre_,progress_,q_,rp_,rt_,rtc_,ruby_,s_,samp_,script_,section_,select_,slot_,small_,source_,span_,strong_,style_,sub_,summary_,sup_,table_,tbody_,td_,template_,textarea_,tfoot_,th_,thead_,time_,title_,tr_,track_,u_,ul_,var_,video_,wbr_
-}
+  }
 })();
